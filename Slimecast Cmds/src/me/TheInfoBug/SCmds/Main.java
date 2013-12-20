df4b99f9aed6f86755deb9666d50d9df693d627f
TheInfoBug <dylanmcpeek12@gmail.com>
2013-12-20 16:35:15 -0600

    Added in /gm command [Untested]

--- Slimecast Cmds/src/me/TheInfoBug/SCmds/Main.java
+++ Slimecast Cmds/src/me/TheInfoBug/SCmds/Main.java
@@ -1,6 +1,7 @@
 package me.TheInfoBug.SCmds;
 
 import me.TheInfoBug.SCmds.commands.commandClear;
+import me.TheInfoBug.SCmds.commands.commandGamemode;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
@@ -16,10 +17,11 @@ public class Main extends JavaPlugin{
 		
 	}
 	
-	
+	//I like using methods. To keep the onEnable() clean 
 	public void setCommands(){
 		
 		getCommand("ci").setExecutor(new commandClear(this));
+		getCommand("gm").setExecutor(new commandGamemode(this));
 		
 	}
 }
---
+++ Slimecast Cmds/src/me/TheInfoBug/SCmds/commands/commandGamemode.java
@@ -0,0 +1,49 @@
+package me.TheInfoBug.SCmds.commands;
+
+import me.TheInfoBug.SCmds.Main;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class commandGamemode implements CommandExecutor {
+
+	Main plugin;
+	
+	public commandGamemode(Main main){
+		this.plugin = main;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("gm")){
+			if(sender instanceof Player){
+				Player p = (Player) sender;
+				if(p.isOp() || p.hasPermission("SCmds.gm")){
+					if(!(args.length < 0)){
+						if(args[0].equalsIgnoreCase("s") || args[0].equals("0")){
+							p.setGameMode(GameMode.SURVIVAL);
+						}else if(args[0].equalsIgnoreCase("c") || args[0].equals("1")){
+							p.setGameMode(GameMode.CREATIVE);
+						}else{
+							sender.sendMessage(ChatColor.GOLD + "Usage: /gm (GameMode)");
+						}
+						
+					}else{
+						sender.sendMessage(ChatColor.GOLD + "Usage: /gm (GameMode)");
+					}
+					
+				}else{
+					sender.sendMessage(ChatColor.GOLD + "[" + ChatColor.GREEN + "SCmds" + ChatColor.GOLD + "] " + ChatColor.GREEN + "You may not use this command!");
+				}
+				
+			}else{
+				sender.sendMessage("Only players may use this command!");
+			}
+		}
+		return true;
+	}
+}
